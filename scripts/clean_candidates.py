3f052c334a31fb7d80daff0c96336b05c52b701a
Nico Schmidt <ps42begu@studserv.uni-leipzig.de>
2022-08-17 00:50:29 +0200

    add cleaned candidates.txt

--- scripts/clean_candidates.py
+++ scripts/clean_candidates.py
@@ -2,24 +2,40 @@ import re
 
 special_chars = ["\)|\*|\+|\||,|/|;|:|@|]|}|<|>|_|%|$|~"]    # find all non ascii characters + special chars
 non_ascii = [r"[^\x00-\x7F]+"]
-other = "com|epl|htm|jpg|org|php"
-html = ["<[^<]+?>"]
-nbsp = [" "]
+other = "com |epl |htm |jpg |org |php "
+html = "<[^<]+?>"
+nbsp = " "
 
-reg = "|".join(html+nbsp)
 regex = "|".join(special_chars+non_ascii)
 
-with open("../data/test.txt", "r", encoding='utf-8') as input_text:
+with open("../data/all2.txt", "r", encoding='utf-8') as input_text:
     input = set(input_text.readlines())
 
 
-with open("../data/finished.txt", "w", encoding='utf-8') as output_text:
-    for line in input:
-        if re.match(reg, line):
-            output_text.writelines(re.sub(reg, "", line))
-        elif re.match(other, line[:3]):
-            output_text.writelines(line[3:])
-        elif re.match(regex, line[:1]):
-            output_text.writelines(line[1:])
-        else:
-            output_text.writelines(line)
+output = []
+append = True
+
+for line in input:
+    if nbsp in line:
+        output.append(line.replace(nbsp, ""))
+        append = False
+
+    if re.match(regex, line[:1]):
+        output.append(line[1:])
+        append = False
+
+    if re.match(html, line):
+        output.append(re.sub(html, "", line))
+        append = False
+
+    if re.match(other, line[:3]):
+        output.append(line[3:])
+        append = False
+
+    if append:
+        output.append(line)
+
+    append = True
+
+with open("../data/candidates.txt", "w", encoding='utf-8') as output_text:
+    output_text.writelines(output)
