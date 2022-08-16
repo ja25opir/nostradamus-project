2149118f5cb6dd6e928ab1124ab75467b6aa3a05
Marvin Müller <marvin.mueller@singular-it.de>
2022-08-16 11:25:03 +0200

    add shuffle, split and label-function to preprocessing script

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -5,29 +5,56 @@ import argparse
 
 
 def import_data(path):
+    print('importing text file...')
     f = open(path, 'r', encoding='utf8')
     lines = f.readlines()
     f.close()
 
-    data = {'candidates': [], 'label': []}
+    data = {'candidate': [], 'label': []}
 
+    print('creating df...')
     for index, line in enumerate(lines):
-        data['candidates'].append(line.strip())
-        data['label'].append(0)
-        if index == 10:
-            break
+        if len(line) > 1:
+            data['candidate'].append(line.strip())
+            data['label'].append(0)
+    df = pd.DataFrame(data=data, columns=['candidate', 'label'])
 
-    df = pd.DataFrame(data=data, columns=['candidates', 'label'])
+    print('saving df as pickle...')
     df.to_pickle('../data/candidates.pkl')
     return df
 
 
+def split_data(size, data):
+    shuffled = data.sample(frac=1).reset_index(drop=True)
+    data_slice = shuffled.loc[:size - 1, :]
+    data_slice_unlabeled = shuffled.loc[size:, :]
+    data_slice_unlabeled.to_pickle('../data/candidates_unlabeled.pkl')
+    return data_slice
+
+
+def label_data(data):
+    for index, row in data.iterrows():
+        print(row['candidate'])
+        label = ''
+        while not label.isnumeric():
+            label = input('label: ')
+        data.at[index, 'label'] = label
+        print('------')
+    data.to_pickle('../data/candidates_labeled.pkl')
+    print(data)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--import_data', nargs='?', action='store')
+    parser.add_argument('--import_data', nargs=1, action='store',
+                        help='Import data from a given path. Tries to use an existing dataset if not provided.')
+    parser.add_argument('--start_labeling', nargs=1, action='store',
+                        help='Slice a set of a given size out of the original dataset and present every sentence for '
+                             'a manual labeling process.')
     args = parser.parse_args()
+
     if args.import_data:
-        candidates = import_data(args.import_data)
+        candidates = import_data(args.import_data[0])
     else:
         try:
             candidates = pd.read_pickle('../data/candidates.pkl')
@@ -35,4 +62,6 @@ if __name__ == '__main__':
             print("No pickled data available. Use --import argument.")
             sys.exit(1)
 
-    print(candidates)
+    if args.start_labeling:
+        candidates_split = split_data(int(args.start_labeling[0]), candidates)
+        label_data(candidates_split)
