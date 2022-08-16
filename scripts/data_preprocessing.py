fbf4aabfd0e9ea5de0e98a81ca81379a1be4265a
Marvin Müller <marvin.mueller@singular-it.de>
2022-08-16 09:52:25 +0200

    update import script

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,16 +1,38 @@
+import sys
+
 import pandas as pd
+import argparse
 
-f = open('../data/all.txt', 'r', encoding='utf8')
-lines = f.readlines()
-f.close()
 
-data = {'candidates': [], 'label': []}
+def import_data(path):
+    f = open(path, 'r', encoding='utf8')
+    lines = f.readlines()
+    f.close()
 
-for index, line in enumerate(lines):
-    data['candidates'].append(line.strip())
-    data['label'].append(0)
-    if index == 10:
-        break
+    data = {'candidates': [], 'label': []}
 
-df = pd.DataFrame(data=data, columns=['candidates', 'label'])
-print(df)
+    for index, line in enumerate(lines):
+        data['candidates'].append(line.strip())
+        data['label'].append(0)
+        if index == 10:
+            break
+
+    df = pd.DataFrame(data=data, columns=['candidates', 'label'])
+    df.to_pickle('../data/candidates.pkl')
+    return df
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--import_data', nargs='?', action='store')
+    args = parser.parse_args()
+    if args.import_data:
+        candidates = import_data(args.import_data)
+    else:
+        try:
+            candidates = pd.read_pickle('../data/candidates.pkl')
+        except FileNotFoundError:
+            print("No pickled data available. Use --import argument.")
+            sys.exit(1)
+
+    print(candidates)
