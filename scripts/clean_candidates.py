20045e712224ac213bffb78b94f16a07b2452138
Nico Schmidt <ps42begu@studserv.uni-leipzig.de>
2022-08-22 18:34:42 +0200

    add arg-parser and error handling

--- scripts/clean_candidates.py
+++ scripts/clean_candidates.py
@@ -1,11 +1,15 @@
+import argparse
 import re
 import os
 
 
 def read_sentence(file):
-    with open(file, "r", encoding="utf-8") as uncleaned_sentence:
-        sentence = uncleaned_sentence.read()
-    return sentence
+    try:
+        with open(file, "r", encoding="utf-8") as uncleaned_sentence:
+            sentence = uncleaned_sentence.read()
+        return sentence
+    except IOError as e:
+        print(f"An error occurred while opening {file}: {e}")
 
 
 def clean_sentence(sentence, filter):
@@ -16,15 +20,28 @@ def clean_sentence(sentence, filter):
 
 
 def write_data(sentence_set, path):
-    cleaned_sentences = open(path+"output.txt", "w", encoding="utf-8")
-    for sentence in sentence_set:
-        cleaned_sentences.writelines(sentence+"\n")
-    cleaned_sentences.close()
+    try:
+        cleaned_sentences = open(path+"output.txt", "w", encoding="utf-8")
+        for sentence in sentence_set:
+            cleaned_sentences.writelines(sentence + "\n")
+        cleaned_sentences.close()
+    except IOError as e:
+        print(f"An error occurred while writing \"output.txt\": {e}")
 
 
 if __name__ == "__main__":
-    input_dir = "../warc-dumps/test_dir/"
-    output_dir = "../warc-dumps/test_dir/"
+    parser = argparse.ArgumentParser(description="Clean candidate sentences for further processing.")
+
+    parser.add_argument("--input_dir", nargs=1, default="/",
+                        help="Specify input directory containing .txt files with uncleaned sentences."
+                             " Default is current directory.")
+    parser.add_argument("--output_dir", nargs=1, default="/",
+                        help="Specify output directory, where the cleaned and combined output.txt is saved to. "
+                             "Default is current directory")
+    args = parser.parse_args()
+
+    input_dir = args.input_dir[0]
+    output_dir = args.output_dir[0]
 
     leading_chars = r"^[^[a-zA-Z1-9(“'\"]*"
     # urls = r"\((?P<url>https?://[^\s]+)\)"
@@ -34,7 +51,9 @@ if __name__ == "__main__":
     sentences = []
 
     for input_file in os.listdir(input_dir):
-        input_sentence = read_sentence(input_dir + input_file)
-        sentences.append(clean_sentence(input_sentence, regex_filter))
+        if input_file.endswith(".txt"):
+            input_sentence = read_sentence(input_dir + input_file)
+            sentences.append(clean_sentence(input_sentence, regex_filter))
 
-    write_data(set(sentences), output_dir)
+    if sentences:
+        write_data(set(sentences), output_dir)
