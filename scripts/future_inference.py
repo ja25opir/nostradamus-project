d29df2695dae7fc27d4daa51077698f1bd08fe33
Klaus Schmidt <ks80wofy@betaweb036.medien.uni-weimar.de>
2022-08-26 16:40:27 +0200

    preliminary inference scripts

---
+++ scripts/future_inference.py
@@ -0,0 +1,36 @@
+
+from small_text.integrations.transformers.datasets import TransformersDataset
+from small_text.active_learner import PoolBasedActiveLearner
+from transformers import AutoTokenizer
+import pandas as pd 
+
+
+def preprocess_data(tokenizer, data, max_length=500):
+    data_out = []
+    for i, doc in enumerate(data):
+        encoded_dict = tokenizer.encode_plus(
+            doc,
+            add_special_tokens=True,
+            padding='max_length',
+            max_length=max_length,
+            return_attention_mask=True,
+            return_tensors='pt',
+            truncation='longest_first'
+        )
+        data_out.append((encoded_dict['input_ids'], encoded_dict['attention_mask'], 0)) #Dummy label, ingore!
+    return TransformersDataset(data_out)
+
+
+if __name__ == "__main__":
+
+    model = PoolBasedActiveLearner.load("models/20220824_76futacc.pkl").classifier 
+    ds_path = "data/candidates_labeled.pkl"
+
+    tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+
+    input_data = pd.read_pickle(ds_path)
+    input_data = input_data["candidate"].values
+    encoded_inputs = preprocess_data(tokenizer, input_data)
+
+    targets = model.predict(encoded_inputs)
+    print(targets)
