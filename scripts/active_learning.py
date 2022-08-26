800e3f5ed7b9ba8c301d516c3816b51b8091d4bc
Klaus Schmidt <ks80wofy@betaweb036.medien.uni-weimar.de>
2022-08-26 17:05:10 +0200

    fixing typo in filename

--- scripts/active_learning.py
+++ scripts/active_learning.py
@@ -1,5 +1,5 @@
 import logging 
-import datetime
+from datetime import datetime as dt
 from functools import partial
 
 
@@ -9,7 +9,7 @@ import pandas as pd
 
 from transformers import AutoTokenizer
 
-from sklearn.metrics import f1_score, classification_report
+from sklearn.metrics import f1_score, classification_report, precision_score
 from sklearn.model_selection import train_test_split
 
 from small_text.active_learner import PoolBasedActiveLearner
@@ -19,6 +19,7 @@ from small_text.integrations.transformers.classifiers.factories import Transform
 from small_text.query_strategies import PoolExhaustedException, EmptyPoolException
 from small_text.query_strategies import RandomSampling, LeastConfidence, PredictionEntropy
 from small_text.integrations.transformers.datasets import TransformersDataset
+from small_text.base import LABEL_IGNORED, LABEL_UNLABELED
 
 #CONFIG
 TRANSFORMER_MODEL = TransformerModelArguments('roberta-base')
@@ -32,11 +33,12 @@ SAVE_BEST = False
 
 
 BEST_MODEL = None 
+f1_score_curve = []
 
 def main():
     # Active learning parameters
     clf_factory = TransformerBasedClassificationFactory(TRANSFORMER_MODEL,
-                                                        len(SPEECH_ACTS),
+                                                        len(LABELS),
                                                         kwargs=dict({'device': 'cuda'}))
 
 
@@ -44,15 +46,16 @@ def main():
     train_labeled, train, test = load_dataset()
     
     tokenizer = AutoTokenizer.from_pretrained(TRANSFORMER_MODEL.model, cache_dir='.cache/')
-    x_train = preprocess_data(tokenizer, train["data"].values, train["target"].values)
-    y_train = train_labeled["target"].values
-    x_test = preprocess_data(tokenizer, test["data"].values, test["target"].values)
+    x_train = preprocess_data(tokenizer, train["candidate"].values, train["label"].values)
+    y_train = train_labeled["label"].values
+    x_test = preprocess_data(tokenizer, test["candidate"].values, test["label"].values)
 
 
     #Active learner
     active_learner = PoolBasedActiveLearner(clf_factory, QUERY_STRATEGY, x_train)
+    print(y_train)
     labeled_indices = initialize_active_learner(active_learner, y_train)
-    print(classification_report(x_test.y, active_learner.classifier.predict(x_test), target_names=SPEECH_ACTS))
+    print(classification_report(x_test.y, active_learner.classifier.predict(x_test), target_names=LABELS))
     save_model(active_learner)
 
     try:
@@ -62,36 +65,26 @@ def main():
     except EmptyPoolException:
         print('Error! No more samples left. (Unlabeled pool is empty)')
     
-    print(classification_report(x_test.y, active_learner.classifier.predict(x_test), target_names=SPEECH_ACTS))
+    print(classification_report(x_test.y, active_learner.classifier.predict(x_test), target_names=LABELS))
 
 
 
 def load_dataset():
 
-    labeled_data = {class_name.lower(): value for class_name, value in zip(SPEECH_ACTS, range(len(SPEECH_ACTS)))}
+    labeled_data = {class_name.lower(): value for class_name, value in zip(LABELS, range(len(LABELS)))}
 
     # load labeled dataset
-    dfs = []
-    for label in labeled_data.keys():
-        with open(f"{label}s.txt") as f:
-            data = f.readlines()
-        data = [line.replace("\n", " ") for line in data]
-        df = pd.DataFrame(columns=["data", "target"])
-        df["data"] = data
-        df["target"] = labeled_data[label]
-        dfs.append(df)
-    dataset = pd.concat(dfs, axis=0, ignore_index=True)
-    train_labeled, test_labeled = train_test_split(dataset, test_size=0.2, stratify=dataset["target"], random_state=42)
+    df_labeled = pd.read_pickle("data/candidates_labeled.pkl")
+    df_labeled["label"] = df_labeled["label"].astype("int64")
+    train_labeled, test_labeled = train_test_split(df_labeled, test_size=0.4, stratify=df_labeled["label"], random_state=42)
 
     # load unlabeled
-    with open("unlabeled.txt") as f:
-        data = f.readlines()
-        data = [line.replace("\n", " ") for line in data]
-        train_unlabeled = pd.DataFrame(columns=["data", "target"])
-        train_unlabeled["data"] = data
-        train_unlabeled["target"] = len(SPEECH_ACTS) #dummy label since TransformersDataset expects it  
+    train_unlabeled = pd.read_pickle("data/candidates_unlabeled.pkl")
+    train_unlabeled["label"] = LABEL_UNLABELED
+    train_unlabeled["label"] = train_unlabeled["label"].astype("int64")
 
     print(len(train_labeled))
+
     return train_labeled, pd.concat([train_labeled, train_unlabeled], axis=0, ignore_index=True), test_labeled 
 
 def save_sample(oracle, sentence):
@@ -122,14 +115,14 @@ def perform_active_learning(active_learner, train, labeled_indices, test, text):
     # Perform [NUM_ITERATIONS] iterations of active learning...
     for i in range(NUM_ITERATIONS):
         # ...where each iteration consists of labelling [QUERY_SAMPLES] samples
-        q_indices = active_learner.query(num_samples=QUERY_SAMPLES)
+        q_indices = active_learner.query(num_samples=NUM_QUERY_SAMPLES)
         
         y = []
         annotations = {"fs":0, "n":1}
-        for q_index in q_indices:
-            print(text[q_index][0])
+        for i, q_index in enumerate(q_indices):
+            print(i, text[q_index][0])
             while True:
-                oracle = input("[f]uture statement, [n]one, [i]gnore")
+                oracle = input("[f]uture statement, [n]one, [i]gnore ")
                 if oracle in list(annotations.keys()):
                     print("Label als", annotations[oracle])
                     if SAVE_SAMPLE:
@@ -138,13 +131,13 @@ def perform_active_learning(active_learner, train, labeled_indices, test, text):
                     y.append(annotations[oracle])
                     break 
                 if oracle == "i":
-                    y.append(None)
+                    y.append(LABEL_IGNORED)
                     print("ignored")
                     break
                 else:
                     print("improper input. Try again!")
                 
-
+        y = np.asarray(y).astype("int16")
         # Return the labels for the current query to the active learner.
 
         active_learner.update(y)
@@ -159,7 +152,8 @@ def perform_active_learning(active_learner, train, labeled_indices, test, text):
 def initialize_active_learner(active_learner, y_train):
     x_indices_initial = random_initialization_balanced(y_train, n_samples=len(y_train))
     y_initial = np.array([y_train[i] for i in x_indices_initial])
-
+    print(x_indices_initial)
+    print(y_initial)
     active_learner.initialize_data(x_indices_initial, y_initial)
 
     return x_indices_initial
@@ -168,13 +162,15 @@ def evaluate(active_learner, train, test):
     y_pred = active_learner.classifier.predict(train)
     y_pred_test = active_learner.classifier.predict(test)
     f1_score_test = f1_score(test.y, y_pred_test, average='macro')
-
+    precission_score_test = precision_score(test.y, y_pred_test, average="macro")
+    with open("scores.txt", "a+") as f:
+        f.write(f"{precission_score_test}\n")
     print('Train accuracy: {:.2f}'.format(
         f1_score(train.y, y_pred, average='macro')))
     print('Test accuracy: {:.2f}'.format(f1_score_test))
     
     if REPORT:
-        print(classification_report(test.y, y_pred_test, target_names=SPEECH_ACTS))
+        print(classification_report(test.y, y_pred_test, target_names=LABELS))
 
     print('---')
 
@@ -192,18 +188,17 @@ def evaluate(active_learner, train, test):
 
 
 def save_model(model):
-    date = datetime.today().strftime("%Y%m%d")
+    date = dt.today().strftime("%Y%m%d")
     name = input("Name of to be saved model[Enter to skip, no save]: ")
     if name != "":
-        model.save(F"../models/{date}_{name}.pkl")
+        model.save(F"./models/{date}_{name}.pkl")
     
     #always save best model
     if BEST_MODEL is not None:
-      BEST_MODEL.save(f"../models/{date}_best.pkl")
+      BEST_MODEL.save(f"./models/{date}_best.pkl")
 
 
 if __name__ == '__main__':
     logging.getLogger('small_text').setLevel(logging.INFO)
     logging.getLogger('transformers.modeling_utils').setLevel(logging.ERROR)
-
     main()
