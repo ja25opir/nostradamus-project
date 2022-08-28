0858828fb629f833ed4bbbc8926b04c60f30f40a
Marvin Müller <marvin.mueller@singular-it.de>
2022-08-28 17:15:45 +0200

    update evaluation script, add figures

--- scripts/data_evaluation.py
+++ scripts/data_evaluation.py
@@ -1,6 +1,23 @@
 import argparse
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+
+
+def plot_emotions_distribution(plt_data, drop_neutral=False):
+    emotions_count = plt_data.value_counts(normalize=True, ascending=True).drop(
+        'neutral') if drop_neutral else plt_data.value_counts(normalize=True, ascending=True)
+    labels = emotions_count.index.tolist()
+    y_pos = np.arange(len(labels))
+    plt.style.use('bmh')
+    plt.title('Distribution after emotion detection exluding "neutral"') if drop_neutral else plt.title(
+        'Distribution after emotion detection')
+    plt.barh(y_pos, emotions_count)
+    plt.yticks(y_pos, labels)
+    plt.xlabel('Percentage')
+    plt.savefig('documentation/figures/emotions_distribution_drop_neutral.png') if drop_neutral else plt.savefig(
+        'documentation/figures/emotions_distribution.png')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -8,11 +25,13 @@ if __name__ == '__main__':
                         help='Analyze data from a given path.')
     args = parser.parse_args()
 
-    data = pd.read_pickle(args.input_file[0]).iloc[:, 2:]
-    max_col = pd.Series(data.idxmax(axis="columns"), name='emotion class')
-    max_vals = data.max(axis=1)
+    data = pd.read_pickle(args.input_file[0])
+    emotions = data.iloc[:, 2:]
+    sentences = data.iloc[:, 0]
+    assigned_emotions = pd.Series(emotions.idxmax(axis='columns'), name='emotion class')
+    max_vals = emotions.max(axis=1)
     low_confidence_idx = max_vals.index[max_vals <= 0.5].tolist()
-    max_col.iloc[low_confidence_idx] = 'undecided'
+    assigned_emotions.iloc[low_confidence_idx] = 'undecided'
 
-    max_col.value_counts().plot(kind="pie")
-    plt.show()
+    plot_emotions_distribution(assigned_emotions, drop_neutral=True)
+    plot_emotions_distribution(assigned_emotions, drop_neutral=False)
