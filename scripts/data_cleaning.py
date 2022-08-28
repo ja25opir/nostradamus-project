c93a37039613bf02b28c8e9911ea538ba748a573
Nico Schmidt <nico.hettstedt@gmx.de>
2022-08-28 22:35:07 +0200

    add docstrings

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -3,17 +3,14 @@ import re
 import os
 
 
-def clean_sentence(sentence):
-    leading_chars = r"^[^[a-zA-Z1-9(“'\"]*"
-    html = r"<[^<]+?>"
-
-    sentence = sentence.replace(" ", " ")
-    sentence = re.sub(leading_chars, "", sentence)
-    sentence = re.sub(html, "", sentence)
-    return sentence
-
-
 def read_sentence(file):
+    """
+    Read textfile and pass every line to clean_sentence method.
+    :param file: str
+        path to textfile with sentences
+    :return: list
+        with cleaned sentences
+    """
     try:
         cleaned_sentences = []
         with open(file, "r", encoding="utf-8") as uncleaned_text:
@@ -25,7 +22,32 @@ def read_sentence(file):
         print(f"An error occurred while opening {file}: {e}")
 
 
+def clean_sentence(sentence):
+    """
+    Cleans sentences from given strings or regexes.
+    :param sentence: str
+        sentence that should be cleaned
+    :return: str
+        cleaned sentence
+    """
+    nbsp = " "
+    leading_chars = r"^[^[a-zA-Z1-9(“'\"]*"
+    html = r"<[^<]+?>"
+
+    sentence = sentence.replace(nbsp, " ")
+    sentence = re.sub(leading_chars, "", sentence)
+    sentence = re.sub(html, "", sentence)
+    return sentence
+
+
 def write_data(sentence_set, output):
+    """
+    Writes cleaned sentences into specified output file.
+    :param sentence_set: set
+        set of sentences without duplicates
+    :param output:
+        path where output file should be saved
+    """
     try:
         cleaned_sentences = open(f"{output}.txt", "w", encoding="utf-8")
         for sentence in sentence_set:
@@ -43,12 +65,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     input_dir = args.clean_data[0]
-    output_name = args.clean_data[1]
+    output_file = args.clean_data[1]
 
     sentences = []
     for input_file in os.listdir(input_dir):
         if input_file.endswith(".txt"):
-            sentences = [*sentences, *read_sentence(input_dir+input_file)]
+            sentences = [*sentences, *read_sentence(input_dir + input_file)]
 
     if sentences:
-        write_data(set(sentences), input_dir+output_name)
+        write_data(set(sentences), input_dir + output_file)
